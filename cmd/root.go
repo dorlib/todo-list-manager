3f8e89815b8823cb7a6655ead91e2fb136718362
dor <dorlibrm@gmail.com>
2023-04-06 18:30:50 +0300

    initial commit

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 todo-list <dorlib318@gmail.com>
+*/
+
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "todo",
+	Short: "TODO is a simple todo list app to manage your tasks",
+	Long: ` TODO is a CLI library for Go that lets you organize you tasks.
+This application is a tool to track and update daily tasks in order 
+to make your day more organized and efficient.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	Version: "1.0.0",
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+}
---
+++ data/data.go
@@ -0,0 +1,5 @@
+package data
+
+func OpenDataBase() {
+	
+}
