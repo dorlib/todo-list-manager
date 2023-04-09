339aed2d013f6d5d00542f55cf1d3d3c9d9261b4
dor <dorlibrm@gmail.com>
2023-04-09 22:42:18 +0300

    move TaskExist to utils

--- data/utils.go
+++ data/utils.go
@@ -2,12 +2,30 @@ package data
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"github.com/jedib0t/go-pretty/v6/table"
 	"os"
+	"strconv"
 )
 
 func printTask(task Task) {
-	timeLeft := task.Deadline.Sub(task.CreatedAt)
+	year, err := strconv.Atoi(task.Deadline.Year)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	month, err := strconv.Atoi(task.Deadline.Month)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	day, err := strconv.Atoi(task.Deadline.Day)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	endingTime := task.CreatedAt.AddDate(year, month, day)
+	timeLeft := endingTime.Sub(task.CreatedAt)
 
 	fmt.Printf("# %d: %v %v %v %v %v %v", task.ID, task.Title, task.Description, task.Priority, task.CreatedAt, task.Deadline, timeLeft)
 }
@@ -25,4 +43,34 @@ func printAllTasks(tasks []Task) {
 
 	t.AppendFooter(table.Row{"Total", 10000})
 	t.Render()
+}
+
+func TaskExistByID(taskID uuid.UUID) bool {
+	var task Task
+
+	tx := DB.First(&task, taskID)
+	if tx != nil {
+		return true
+	}
+
+	return false
+}
+
+func TaskExistByName(taskName string) bool {
+	var task Task
+
+	tx := DB.Where("name = ?", taskName).First(&task)
+	if tx != nil {
+		return true
+	}
+
+	return false
+}
+
+func CheckLegalPriority(priority string) bool {
+	if priority == Critical || priority == VeryHigh || priority == High || priority == Medium || priority == Low {
+		return true
+	}
+
+	return false
 }
