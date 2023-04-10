7bb9cd6f244d19ea04f0a5a6eb3ace03cd137ca7
dor <dorlibrm@gmail.com>
2023-04-10 21:35:06 +0300

    add delete cmd

--- cmd/delete.go
+++ cmd/delete.go
@@ -6,6 +6,8 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/google/uuid"
+	"todo/data"
 
 	"github.com/spf13/cobra"
 )
@@ -20,8 +22,28 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("delete called")
+
+		taskTitle, err := cmd.Flags().GetString("taskTitle")
+		if err != nil {
+			fmt.Printf("error while parsing flag: %v", err)
+		}
+
+		taskID, err := cmd.Flags().GetString("taskID")
+		if err != nil {
+			fmt.Printf("error while parsing flag: %v", err)
+		}
+
+		if taskTitle != "" {
+			data.DeleteTaskByTitle(taskTitle)
+		}
+
+		if taskID != "" {
+			data.DeleteTaskByID(uuid.MustParse(taskID))
+		}
+
 	},
 }
 
@@ -35,6 +57,10 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	printCmd.PersistentFlags().StringP("taskTitle", "t", "", "a"+"delete by the task's title")
+	printCmd.PersistentFlags().StringP("taskID", "i", "", "delete by the task's ID")
+	printCmd.PersistentFlags().StringP("all", "a", "", "delete all tasks")
 
 	rootCmd.AddCommand(deleteCmd)
 }
--- data/resolvers.go
+++ data/resolvers.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"fmt"
 	"github.com/google/uuid"
 )
 
@@ -9,8 +10,34 @@ func CreateTask(title, description, priority string, deadline Date) {
 	DB.Create(&task)
 }
 
-func DeleteTask(taskID uuid.UUID) {
-	DB.Delete("ID = ?", taskID)
+func DeleteTaskByID(taskID uuid.UUID) {
+	if TaskExistByID(taskID) {
+		DB.Delete(&Task{}, taskID)
+		fmt.Printf("deleted task: %v", taskID)
+		return
+	}
+
+	fmt.Printf("there is no tasks with the given ID: %v", taskID)
+}
+
+func DeleteTaskByTitle(taskTitle string) {
+	var tasks []Task
+
+	DB.Where("Title = ?", taskTitle).Find(&tasks)
+
+	if len(tasks) > 1 {
+		fmt.Printf("more than one task with the same title %v exists", taskTitle)
+		return
+	}
+
+	if len(tasks) < 1 {
+		fmt.Printf("there is no tasks with the given title: %v", taskTitle)
+		return
+	}
+
+	DB.Where("name = ?", taskTitle).Delete(&tasks)
+	fmt.Printf("deleted task: %v", taskTitle)
+
 }
 
 func PrintTask(taskID uuid.UUID) {
@@ -22,7 +49,7 @@ func PrintTask(taskID uuid.UUID) {
 }
 
 func PrintTaskByName(taskName string) {
-	var task Task
+	task := Task{}
 
 	DB.Where("name = ?", taskName).First(&task)
 
