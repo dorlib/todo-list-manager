edc2fb950d099dcd857cd9ed6d211147a0177828
dor <dorlibrm@gmail.com>
2023-04-10 22:17:15 +0300

    add delete cmd

--- cmd/delete.go
+++ cmd/delete.go
@@ -49,15 +49,12 @@ to quickly create a Cobra application.`,
 
 func init() {
 	// Here you will define your flags and configuration settings.
-
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
 	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 	printCmd.PersistentFlags().StringP("taskTitle", "t", "", "a"+"delete by the task's title")
 	printCmd.PersistentFlags().StringP("taskID", "i", "", "delete by the task's ID")
 	printCmd.PersistentFlags().StringP("all", "a", "", "delete all tasks")
