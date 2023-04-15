cd362a522e8f979a3395c64619fce526aa0ece11
dor <dorlib318@gmail.com>
2023-04-15 13:32:23 +0300

    fix database connection

--- data/data.go
+++ data/data.go
@@ -3,14 +3,18 @@ package data
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func OpenDataBase() {
+	var err error
 	dsn := "root:12345678@tcp(127.0.0.1:3306)/todo?charset=utf8mb4&parseTime=True&loc=Local"
 
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
 		panic("failed to connect to database")
 	}
