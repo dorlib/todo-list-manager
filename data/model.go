05da7925dbe4561a5ec393b34701514e6d486339
dor <dorlib318@gmail.com>
2023-05-01 22:08:55 +0300

    declare iota for status in model

--- data/model.go
+++ data/model.go
@@ -57,3 +57,24 @@ const (
 	Medium   string = "medium"
 	Low      string = "low"
 )
+
+type Status int
+
+const (
+	Todo Status = iota
+	InProgress
+	Done
+)
+
+// returns the corresponding string value to the Card enum
+func (s Status) String() string {
+	switch s {
+	case Todo:
+		return "todo"
+	case InProgress:
+		return "in progress"
+	case Done:
+		return "done"
+	}
+	return "Unknown status"
+}
