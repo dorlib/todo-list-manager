d0b1d5edbc086cc0113b7eafc76ce80d83f25f59
dor <dorlib318@gmail.com>
2023-05-03 23:29:18 +0300

    and new commands for managing users and groups

--- cmd/group/add-user.go
+++ cmd/group/add-user.go
@@ -1,17 +1,17 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
-package cmd
+package group
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"todo/cmd"
 )
 
-// initCmd represents the init command.
-var initCmd = &cobra.Command{
-	Use:   "init",
+// addUserCmd represents the addUser command
+var addUserCmd = &cobra.Command{
+	Use:   "add-user",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -20,7 +20,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
+		fmt.Println("addUser called")
 	},
 }
 
@@ -28,9 +28,9 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// addUserCmd.PersistentFlags().String("foo", "", "A help for foo")
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	rootCmd.AddCommand(initCmd)
+	// addUserCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cmd.RootCmd.AddCommand(addUserCmd)
 }
---
+++ cmd/group/update-group.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package group
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"todo/cmd"
+)
+
+// updateGroupCmd represents the updateGroup command
+var updateGroupCmd = &cobra.Command{
+	Use:   "update-group",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("updateGroup called")
+	},
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// updateGroupCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// updateGroupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cmd.RootCmd.AddCommand(updateGroupCmd)
+}
