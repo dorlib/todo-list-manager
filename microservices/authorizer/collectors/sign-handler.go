0e29fec9d18820ada1556978d5fd62872975957c
dor <dorlib318@gmail.com>
2023-06-01 23:43:20 +0300

    add sign handler wip

--- microservices/authorizer/collectors/sign-handler.go
+++ microservices/authorizer/collectors/sign-handler.go
@@ -1,1 +1,50 @@
 package collectors
+
+import (
+	"authorizer/models"
+	"authorizer/services"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"log"
+	"net/http"
+)
+
+type Sign struct {
+	logger       *log.Logger
+	flags        *models.Flags
+	loginService *services.Login
+}
+
+func NewSign(l *log.Logger, f *models.Flags) *Sign {
+	signService := services.NewSign(l, f)
+	return &Sign{
+		logger:       l,
+		flags:        f,
+		loginService: signService,
+	}
+}
+
+func (s *Sign) Sign(ctx *gin.Context) {
+	var body struct {
+		UserName string
+		Password String
+	}
+	if ctx.Bind(&body) != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	// create user (consider implement here and move from data).
+}
