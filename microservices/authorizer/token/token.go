d1103e6b9d675a111a986ca3b5b8acaf3aabbbb7
dor <dorlib318@gmail.com>
2023-06-08 23:59:19 +0300

    fix lint issues

--- microservices/authorizer/token/token.go
+++ microservices/authorizer/token/token.go
@@ -13,9 +13,9 @@ import (
 )
 
 const (
-	CorruptToken = "Corrupt Token"
-	InvalidToken = "Invalid Token"
-	ExpiredToken = "Expired Token"
+	CORRUPT_TOKEN = "Corrupt Token"
+	INVALID_TOKEN = "Invalid Token"
+	EXPIRED_TOKEN = "Expired Token"
 )
 
 type ClaimsMap struct {
@@ -24,8 +24,9 @@ type ClaimsMap struct {
 	Exp string
 }
 
-// GetSecret fetches the value for the JWT_SECRET from the environment variable
+// GetSecret fetches the value for the JWT_SECRET from the environment variable.
 func GetSecret() string {
+
 	return os.Getenv("JWT_SECRET")
 }
 
@@ -37,6 +38,7 @@ func GenerateToken(header string, payload map[string]string, secret string) (str
 	payloadstr, err := json.Marshal(payload)
 	if err != nil {
 		fmt.Println("Error generating Token")
+
 		return string(payloadstr), err
 	}
 
@@ -46,25 +48,28 @@ func GenerateToken(header string, payload map[string]string, secret string) (str
 
 	h.Write([]byte(unsignedStr))
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
-
 	tokenStr := message + "." + signature
+
 	return tokenStr, nil
 }
 
-// ValidateToken is validating the token
+// ValidateToken is validating the token.
 func ValidateToken(token string, secret string) (bool, error) {
 	splitToken := strings.Split(token, ".")
 	if len(splitToken) != 3 {
-		return false, errors.New(CorruptToken)
+
+		return false, errors.New(CORRUPT_TOKEN)
 	}
 
 	header, err := base64.StdEncoding.DecodeString(splitToken[0])
 	if err != nil {
+
 		return false, err
 	}
 
 	payload, err := base64.StdEncoding.DecodeString(splitToken[1])
 	if err != nil {
+
 		return false, err
 	}
 
@@ -75,18 +80,21 @@ func ValidateToken(token string, secret string) (bool, error) {
 	signature := base64.StdEncoding.EncodeToString(h.Sum(nil))
 
 	if signature != splitToken[2] {
-		return false, errors.New(InvalidToken)
+
+		return false, errors.New(INVALID_TOKEN)
 	}
 
 	var payloadMap ClaimsMap
 
 	err = json.Unmarshal(payload, &payloadMap)
 	if err != nil {
+
 		return false, err
 	}
 
 	if payloadMap.Exp < fmt.Sprint(time.Now().Unix()) {
-		return false, errors.New(ExpiredToken)
+
+		return false, errors.New(EXPIRED_TOKEN)
 	}
 
 	return true, nil
