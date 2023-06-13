3f37e718bec1c7bbeff320c30733afd0ba8e1b20
dor <dorlib318@gmail.com>
2023-06-13 23:34:44 +0300

    mongodb connection

--- microservices/authorizer/data/data.go
+++ microservices/authorizer/data/data.go
@@ -1,6 +1,10 @@
 package data
 
 import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"gorm.io/gorm"
 )
 
@@ -8,7 +12,16 @@ var DB *gorm.DB
 
 // OpenDataBase should connect to mongoDB to manage users and api-keys.
 func OpenDataBase() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		panic(err)
+	}
 
+	fmt.Printf("connected to mongodb: %v", client)
+
+	usersCollection := client.Database("testing").Collection("users")
+
+	fmt.Printf("users collection: %v", usersCollection)
 }
 
 // BeforeSave is a gorm hook in order to initiate the deadline field.
