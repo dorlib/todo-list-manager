f4b665307bf04321ff1552ae22484ce0fe97b0a9
dor <dorlib318@gmail.com>
2023-06-21 23:54:48 +0300

    add resolvers for mongoDB

--- microservices/authorizer/data/resolvers.go
+++ microservices/authorizer/data/resolvers.go
@@ -1,1 +1,40 @@
 package data
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func addUserKey(username string, key string) {
+	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+
+	result, err := UsersCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.InsertedID)
+}
+
+func checkKeyExist(key string) {
+	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+
+	result, err := UsersCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.InsertedID)
+}
+
+func getUserKey(username string, key string) {
+	user := bson.D{{"fullName", "User 1"}, {"age", 30}}
+
+	result, err := UsersCollection.InsertOne(context.TODO(), user)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result.InsertedID)
+}
