a7ea7bbc9c711354a5366ceba03c69decabc7896
dor <dorlib318@gmail.com>
2023-06-21 23:54:16 +0300

    add util function to authentication service

--- microservices/authorizer/handlers/utils.go
+++ microservices/authorizer/handlers/utils.go
@@ -1,1 +1,24 @@
 package handlers
+
+import (
+	"errors"
+	"github.com/shadowshot-x/micro-product-go/authservice/data"
+)
+
+// searches the user in the database.
+func validateUser(email string, passwordHash string) (bool, error) {
+	usr, exists := data.GetUserObject(email)
+	if !exists {
+
+		return false, errors.New("user does not exist")
+	}
+
+	passwordCheck := usr.ValidatePasswordHash(passwordHash)
+
+	if !passwordCheck {
+
+		return false, nil
+	}
+
+	return true, nil
+}
