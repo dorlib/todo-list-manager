2808ef20193d958fd7d1d6601d235b172f998561
dor <dorlib318@gmail.com>
2023-06-28 23:22:23 +0300

    fix lint issues

--- microservices/authorizer/handlers/signup-handler.go
+++ microservices/authorizer/handlers/signup-handler.go
@@ -8,35 +8,53 @@ import (
 func SignupHandler(rw http.ResponseWriter, r *http.Request) {
 	if _, ok := r.Header["Email"]; !ok {
 		rw.WriteHeader(http.StatusBadRequest)
-		rw.Write([]byte("Email Missing"))
+		_, err := rw.Write([]byte("Email Missing"))
+		if err != nil {
+			return
+		}
 
 		return
 	}
 
 	if _, ok := r.Header["Username"]; !ok {
 		rw.WriteHeader(http.StatusBadRequest)
-		rw.Write([]byte("Username Missing"))
+		_, err := rw.Write([]byte("Username Missing"))
+		if err != nil {
+			return
+		}
 
 		return
 	}
 
 	if _, ok := r.Header["Passwordhash"]; !ok {
 		rw.WriteHeader(http.StatusBadRequest)
-		rw.Write([]byte("Passwordhash Missing"))
+		_, err := rw.Write([]byte("Passwordhash Missing"))
+		if err != nil {
+			return
+		}
 
 		return
 	}
 
 	if _, ok := r.Header["Fullname"]; !ok {
 		rw.WriteHeader(http.StatusBadRequest)
-		rw.Write([]byte("Fullname Missing"))
+		_, err := rw.Write([]byte("Fullname Missing"))
+		if err != nil {
+			return
+		}
 
 		return
 	}
 
 	// validate and then add the user
-	rw.Write([]byte(r.Header["Passwordhash"][0]))
+	_, err := rw.Write([]byte(r.Header["Passwordhash"][0]))
+	if err != nil {
+		return
+	}
 
 	rw.WriteHeader(http.StatusOK)
-	rw.Write([]byte("User Created"))
+	_, err := rw.Write([]byte("User Created"))
+	if err != nil {
+		return
+	}
 }
