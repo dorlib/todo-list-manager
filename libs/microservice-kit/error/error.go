d3e55fe892d40dd7fbaa625ec8ad49f656cdfba9
dor <dorlib318@gmail.com>
2023-10-05 21:18:28 +0300

    add error constructors

--- libs/microservice-kit/error/error.go
+++ libs/microservice-kit/error/error.go
@@ -1,6 +1,10 @@
 package error
 
-import "strings"
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"strings"
+)
 
 type AppError struct {
 	Code   int    `json:"-"`
@@ -31,4 +35,58 @@ func (a *AppError) Error() string {
 	}
 
 	return sb.String()
+}
+
+func (a *AppError) IsResourceNotFound() bool {
+	return a.Code == http.StatusNotFound
+}
+
+func (a *AppError) IsInternalServerError() bool {
+	return a.Code == http.StatusInternalServerError
+}
+
+func (a *AppError) IsInvalidRequest() bool {
+	return a.Code == http.StatusBadRequest
+}
+
+func (a *AppError) IsConflictError() bool {
+	return a.Code == http.StatusConflict
+}
+
+func NewResourceNotFound(bodyError string) *AppError {
+	return &AppError{
+		Code:   http.StatusNotFound,
+		Title:  "resource not found",
+		Detail: bodyError,
+	}
+}
+
+func NewInternalServerError() *AppError {
+	return &AppError{
+		Code:  http.StatusInternalServerError,
+		Title: DefaultErrorMsg,
+	}
+}
+
+func NewInvalidRequest(bodyError string) *AppError {
+	return &AppError{
+		Code:   http.StatusBadRequest,
+		Title:  "Invalid Request",
+		Detail: bodyError,
+	}
+}
+
+func NewConflictError(bodyError string) *AppError {
+	return &AppError{
+		Code:   http.StatusConflict,
+		Title:  "Conflict",
+		Detail: bodyError,
+	}
+}
+
+func NewHTTPErrorToAppError(echoErr *echo.HTTPError) *AppError {
+	return &AppError{
+		Code:  echoErr.Code,
+		Title: echoErr.Message.(string),
+	}
 }
