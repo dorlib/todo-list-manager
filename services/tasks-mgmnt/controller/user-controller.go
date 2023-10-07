e6078ebb43dab53e1f190edb5386d3574d0eaa6e
dor <dorlib318@gmail.com>
2023-10-07 20:11:21 +0300

    update controllers

--- services/tasks-mgmnt/controller/user-controller.go
+++ services/tasks-mgmnt/controller/user-controller.go
@@ -1,9 +1,13 @@
 package controller
 
 import (
+	Ctx "github.com/dorlib/todo-list-manager/libs/microservice-development-kit/ctx"
+	"github.com/dorlib/todo-list-manager/libs/microservice-development-kit/validator"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog"
 	"net/http"
+	"tasks-mgmnt/model/request"
+	"tasks-mgmnt/service"
 )
 
 type UserController struct {
@@ -21,12 +25,12 @@ func NewUserController(log *zerolog.Logger, validator *validator.RequestValidato
 }
 
 func (u UserController) CreateUser(c echo.Context) error {
-	req, err := request.BindAndValidateCreateUserRequest(c, u.validator)
+	req, err := request.BindAndValidateCreateRequest(c, u.validator)
 	if err != nil {
 		return err
 	}
 
-	res, err := u.userService.CreateOne(c.Request().Context(), req)
+	res, err := u.userService.CreateUser(c.Request().Context(), Ctx.FromEchoContext(c), req)
 	if err != nil {
 		return err
 	}
@@ -35,7 +39,7 @@ func (u UserController) CreateUser(c echo.Context) error {
 }
 
 func (u UserController) GetUsers(c echo.Context) error {
-	res, err := u.userService.GetMany(c.Request().Context())
+	res, err := u.userService.GetUsers(c.Request().Context(), Ctx.FromEchoContext(c))
 	if err != nil {
 		return err
 	}
@@ -44,12 +48,12 @@ func (u UserController) GetUsers(c echo.Context) error {
 }
 
 func (u UserController) GetUser(c echo.Context) error {
-	req, err := request.BindAndValidateGetUserRequest(c)
+	req, err := request.BindAndValidateGetRequest(c, u.validator)
 	if err != nil {
 		return err
 	}
 
-	res, err := u.userService.GetOne(c.Request().Context(), req)
+	res, err := u.userService.GetUser(c.Request().Context(), Ctx.FromEchoContext(c), req.ID)
 	if err != nil {
 		return err
 	}
@@ -58,26 +62,26 @@ func (u UserController) GetUser(c echo.Context) error {
 }
 
 func (u UserController) DeleteUser(c echo.Context) error {
-	req, err := request.BindAndValidateDeleteUserRequest(c)
+	req, err := request.BindAndValidateDeleteRequest(c, u.validator)
 	if err != nil {
 		return err
 	}
 
-	err := u.userService.Delete(c.Request().Context(), req)
+	res, err := u.userService.DeleteUser(c.Request().Context(), Ctx.FromEchoContext(c), req.ID)
 	if err != nil {
 		return err
 	}
 
-	return c.NoContent(http.StatusNoContent)
+	return c.JSON(http.StatusOK, res)
 }
 
 func (u UserController) UpdateUser(c echo.Context) error {
-	req, err := request.BindAndValidateUpdateUserRequest(c)
+	req, err := request.BindAndValidateUpdateRequest(c, u.validator)
 	if err != nil {
 		return err
 	}
 
-	res, err := u.userService.Update(c.Request().Context(), req)
+	res, err := u.userService.UpdateUser(c.Request().Context(), Ctx.FromEchoContext(c), req)
 	if err != nil {
 		return err
 	}
